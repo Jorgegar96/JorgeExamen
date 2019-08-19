7e55996f978cc021e507b78988c2b75072ae31bb
Jorge Garcia <jorgegar896@gmail.com>
2019-08-18 20:59:37 -0600

    Updates

---
+++ precache-manifest.c6aa0031ea7b07efe36e0807c53d0ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7da9777427ac4dc210b308709c33c11",
+    "url": "/JorgeExamen/index.html"
+  },
+  {
+    "revision": "a481975786fc101da9bf",
+    "url": "/JorgeExamen/static/css/main.2ea4d2d8.chunk.css"
+  },
+  {
+    "revision": "cd60b952aa5e285f9200",
+    "url": "/JorgeExamen/static/js/2.4a3defef.chunk.js"
+  },
+  {
+    "revision": "a481975786fc101da9bf",
+    "url": "/JorgeExamen/static/js/main.eb3f1c35.chunk.js"
+  },
+  {
+    "revision": "c4cffa8c01ca990b9c21",
+    "url": "/JorgeExamen/static/js/runtime~main.2c575f1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JorgeExamen/static/media/logo.5d5d9eef.svg"
+  }
+]);
